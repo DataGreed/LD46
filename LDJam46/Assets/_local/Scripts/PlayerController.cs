4adf5c655f405bd4a3290a78e0b705f8dbdeac1e
Alexey Strelkov <cpr.al.exe@gmail.com>
2020-04-18 17:42:59 +0100

    simple player movement / disabled gravity

---
+++ LDJam46/Assets/_local/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    void FixedUpdate()
+    {
+       
+
+        // update speed based on movement input
+        Vector2 moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        rb.velocity = moveDirection * speed;
+
+        
+        
+
+        // //fire if the fire button is pressed
+        // if(Input.GetButton("Fire1"))
+        // {
+        //     Attack();
+        // }
+        
+
+        //enable/disable running animation
+        // we compare to epsilon, not to zero, so we don't get any floating points errors
+        if (System.Math.Abs(moveDirection.x) > Mathf.Epsilon || System.Math.Abs(moveDirection.y) > Mathf.Epsilon)
+        {
+            
+        }
+        else
+        {
+            
+        }
+    }
+    
+
+}
