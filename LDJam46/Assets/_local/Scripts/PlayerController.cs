735ccf885e9f45aef0677507455efd8ce7209d5f
Alexey Strelkov <cpr.al.exe@gmail.com>
2020-04-18 19:21:35 +0100

    evasion and attack timers

--- LDJam46/Assets/_local/Scripts/PlayerController.cs
+++ LDJam46/Assets/_local/Scripts/PlayerController.cs
@@ -3,103 +3,168 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace MyNamespace
 {
 
-    public float speed = 1.0f;
-    public bool changeVelocity;
-    
-    private Rigidbody2D rb;
 
-    private Vector2 moveDirection;
-
-    [Header("Links to scene objects")]
-    public GameObject ambientLight;
-    public GameObject torchLight;
-
-    private void Start()
+    public class PlayerController : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();   
+
+        private float speed = 1.0f;
         
-        //from the start the torch is not lit
-        ambientLight.SetActive(true);
-        torchLight.SetActive(false);
-    }
+        public float walkingSpeed = 1.0f;
+        public float evasionSpeed = 3.0f;
+        
+        public bool changeVelocity;
+        public float evasionTimeOut = 0.5f;
+        public float attackTimeOut = 0.3f;
+        
+        
+        public PlayerState state { get; private set; }
 
-    private void Update()
-    {
-        //input processing
-        moveDirection.x = Input.GetAxis("Horizontal");
-        moveDirection.y = Input.GetAxis("Vertical");
-        
-        //TODO: normalize?
-        
-        //fire if the fire button is pressed
-        if(Input.GetButtonDown("Fire1"))
-        {
-            Attack();
-        }
-        
-        //FIXME: should only react on pressing once, not holding 
-        //fire if the fire button is pressed
-        if(Input.GetButtonDown("Fire2"))
-        {
-            Evade();
-        }
-        
-        if(Input.GetButtonDown("Fire3"))
-        {
-            LightTorch();
-        }
-    }
+        [Header("Links to scene objects")] public GameObject ambientLight;
+        public GameObject torchLight;
 
-    void FixedUpdate()
-    {
-        // update speed based on movement input
-        if (changeVelocity)
+        private Rigidbody2D rb;
+
+        private Vector2 moveDirection;
+        private float timeBeforeEvasion;
+        private float timeBeforeAttack;
+
+
+        private void Start()
         {
-            rb.velocity = moveDirection * speed;
-        }
-        else
-        {
-            rb.MovePosition(rb.position + moveDirection * speed  * Time.fixedDeltaTime);
+            rb = GetComponent<Rigidbody2D>();
+
+            //from the start the torch is not lit
+            ambientLight.SetActive(true);
+            torchLight.SetActive(false);
+
+            state = PlayerState.Idle;
         }
 
-        
-        //enable/disable running animation
-        // we compare to epsilon, not to zero, so we don't get any floating points errors
-        if (System.Math.Abs(moveDirection.x) > Mathf.Epsilon || System.Math.Abs(moveDirection.y) > Mathf.Epsilon)
+        private void Update()
         {
-            
+            AdvanceTimers();
+
+            //input processing
+            moveDirection.x = Input.GetAxis("Horizontal");
+            moveDirection.y = Input.GetAxis("Vertical");
+
+            //TODO: normalize?
+
+            //fire if the fire button is pressed
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (timeBeforeAttack<=0)
+                {
+                    Attack();
+                    timeBeforeAttack = attackTimeOut;
+                }
+                
+            }
+
+            //FIXME: should only react on pressing once, not holding 
+            //fire if the fire button is pressed
+            if (Input.GetButtonDown("Fire2"))
+            {
+                if (timeBeforeEvasion <= 0)
+                {
+                    Evade();
+                    timeBeforeEvasion = evasionTimeOut;
+                }
+            }
+
+            if (Input.GetButtonDown("Fire3"))
+            {
+                LightTorch();
+            }
         }
-        else
+
+        void AdvanceTimers()
         {
-            
+            if (timeBeforeEvasion > 0)
+            {
+                timeBeforeEvasion -= Time.deltaTime;
+            }
+
+            if (timeBeforeAttack > 0)
+            {
+                timeBeforeAttack -= Time.deltaTime;
+            }
         }
+
+        void FixedUpdate()
+        {
+            if (state != PlayerState.Evading)
+            {
+                // update speed based on movement input
+                if (changeVelocity)
+                {
+                    rb.velocity = moveDirection * speed;
+                }
+                else
+                {
+                    rb.MovePosition(rb.position + moveDirection * speed * Time.fixedDeltaTime);
+                }
+
+
+                //enable/disable running animation
+                // we compare to epsilon, not to zero, so we don't get any floating points errors
+                if (System.Math.Abs(moveDirection.x) > Mathf.Epsilon ||
+                    System.Math.Abs(moveDirection.y) > Mathf.Epsilon)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        void Attack()
+        {
+            print("Player attacks");
+        }
+
+        private void Evade()
+        {
+            print("Player evades");
+            speed = evasionSpeed;
+        }
+
+        /*
+         * Must be called from animation
+         */
+        public void OnEvasionEnded()
+        {
+            state = PlayerState.Idle;
+            speed = walkingSpeed;
+        }
+
+        private void LightTorch()
+        {
+            print("Trying to light torch");
+            ambientLight.SetActive(false);
+            torchLight.SetActive(true);
+        }
+
+        private void ExtinguishTorch()
+        {
+            print("Extinguishing torch");
+            ambientLight.SetActive(true);
+            torchLight.SetActive(false);
+        }
+
     }
 
-    void Attack()
+    public enum PlayerState
     {
-        print("Player attacks");
-    }
-    
-    private void Evade()
-    {
-        print("Player evades");
-    }
-    
-    private void LightTorch()
-    {
-        print("Trying to light torch");
-        ambientLight.SetActive(false);
-        torchLight.SetActive(true);
+        Idle = 1,
+        Walking = 2,
+        Evading = 3,
+        Attacking = 4
     }
 
-    private void ExtinguishTorch()
-    {
-        print("Extinguishing torch");
-        ambientLight.SetActive(true);
-        torchLight.SetActive(false);
-    }
-    
 }
